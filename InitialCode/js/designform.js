9c038558072ae720f5fcb808cbcdc75d3bb8583a
XuHuaYang <31716840+XuHuaYang@users.noreply.github.com>
2018-03-11 15:34:45 +0800

    convert questionnaire to JSON

--- InitialCode/js/designform.js
+++ InitialCode/js/designform.js
@@ -1,6 +1,7 @@
 document.getElementById("textTitle").innerHTML="Enter you form title here";
 var AddButton = document.getElementById("addBox");
 var DeleteButton = document.getElementById("deleteBox");
+var SubmitButton = document.getElementById("submitBox");
 
 var ourHeadline = document.getElementById("our-headline");
 var listItems = document.getElementById("our-list").getElementsByTagName("li");
@@ -23,4 +24,14 @@ DeleteButton.addEventListener("click",deleteTextBox);
 function deleteTextBox(){
 	var list=document.getElementById("text");
 	list.removeChild(list.childNodes[0]);
+}
+
+SubmitButton.addEventListener("click",submitMsg);
+function submitMst(){
+    var data = new Array;
+    data["header"] = "";
+    for(var i = 0;i < listItems.length;i++){
+        data[i] = "";
+    }
+    var myJSON = JSON.stringify(data);//将dictionary转换成JSON
 }
