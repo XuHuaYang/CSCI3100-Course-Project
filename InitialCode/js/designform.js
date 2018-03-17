f07576f7acf19168a3e16a9582dde72196a0b483
wengzichen <w13732427267@gmail.com>
2018-03-18 00:26:17 +0800

    change

--- InitialCode/js/designform.js
+++ InitialCode/js/designform.js
@@ -6,14 +6,115 @@ var SubmitButton = document.getElementById("submitBox");
 
 AddButton.addEventListener("click",addTextBox);
 function addTextBox() {
-	var questionset=document.createElement("input");
-	document.getElementById("newquestionarea").appendChild(questionset);
+	var questionbox=document.createElement("input");
+	document.getElementById("newquestionarea").appendChild(questionbox);
+	var blank=document.createElement("p");
+	document.getElementById("newquestionarea").appendChild(blank);
+	var questionbox=document.createElement("input");
+	document.getElementById("newquestionarea").appendChild(questionbox);
+	var blank=document.createElement("p");
+	document.getElementById("newquestionarea").appendChild(blank);
+	var questionbox=document.createElement("input");
+	document.getElementById("newquestionarea").appendChild(questionbox);
+	var blank=document.createElement("p");
+	document.getElementById("newquestionarea").appendChild(blank);
+	var questionbox=document.createElement("input");
+	document.getElementById("newquestionarea").appendChild(questionbox);
+	var blank=document.createElement("p");
+	document.getElementById("newquestionarea").appendChild(blank);
+	var questionbox=document.createElement("input");
+	document.getElementById("newquestionarea").appendChild(questionbox);
+	var blank=document.createElement("p");
+	document.getElementById("newquestionarea").appendChild(blank);
+	
+	
+	var question=document.createTextNode("Question");
+	document.getElementById("newquestiontitlearea").appendChild(question);
+	var blank=document.createElement("p");
+	document.getElementById("newquestiontitlearea").appendChild(blank);
+	var ChoiceA=document.createTextNode("Choice A");
+	document.getElementById("newquestiontitlearea").appendChild(ChoiceA);
+	var blank=document.createElement("p");
+	document.getElementById("newquestiontitlearea").appendChild(blank);
+	var ChoiceB=document.createTextNode("Choice B");
+	document.getElementById("newquestiontitlearea").appendChild(ChoiceB);
+	var blank=document.createElement("p");
+	document.getElementById("newquestiontitlearea").appendChild(blank);
+	var ChoiceC=document.createTextNode("Choice C");
+	document.getElementById("newquestiontitlearea").appendChild(ChoiceC);
+	var blank=document.createElement("p");
+	document.getElementById("newquestiontitlearea").appendChild(blank);
+	var ChoiceD=document.createTextNode("Choice D");
+	document.getElementById("newquestiontitlearea").appendChild(ChoiceD);
+	var blank=document.createElement("p");
+	document.getElementById("newquestiontitlearea").appendChild(blank);
+
 }
 
 DeleteButton.addEventListener("click",deleteTextBox);
 function deleteTextBox(){
-	var list=document.getElementById("text");
-	list.removeChild(list.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	var boxlist=document.getElementById("newquestionarea");
+	n=boxlist.length;
+	boxlist.removeChild(boxlist.childNodes[0]);
+	
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+	var titlelist=document.getElementById("newquestiontitlearea");
+	n=titlelist.length;
+	titlelist.removeChild(titlelist.childNodes[0]);
+
 }
 
 SubmitButton.addEventListener("click",submitMsg);
