2f97f0aa022e0a073b555efb4a5b7eb87fc24a18
XuHuaYang <31716840+XuHuaYang@users.noreply.github.com>
2018-03-17 21:44:25 +0800

    add questionnaire table

--- InitialCode/js/main.js
+++ InitialCode/js/main.js
@@ -140,8 +140,7 @@ jQuery(document).ready(function( $ ) {
   // Porfolio isotope and filter
   var portfolioIsotope = $('.portfolio-container').isotope({
     itemSelector: '.portfolio-item',
-    //layoutMode: 'fitRows'
-    layoutMode:'vertical'
+    layoutMode: 'fitRows'
   });
 
   $('#portfolio-flters li').on( 'click', function() {
