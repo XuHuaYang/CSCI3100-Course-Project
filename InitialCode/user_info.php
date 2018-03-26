54d367629141c06aabc69c30d04f0d08bb5da4a5
lisy45 <31823638+lisy45@users.noreply.github.com>
2018-03-27 00:04:03 +0800

    修改了返回的json值

--- InitialCode/user_info.php
+++ InitialCode/user_info.php
@@ -1,12 +1,13 @@
 <?php
     header('Content-Type: application/json');
     header('Content-Type: text/html;charset=utf-8');
-	$userID = 1;
+    $id = $_POST["id"];
+
     $userName = "LSY";
     $userSex = "male";
     $userAge = 18;
     $userPoint = 100;
-    $json_arr = array("userID"=>$userID,"userName"=>$userName,"userSex"=>$userSex,"userAge"=>$userAge,"userPoint"=>$userPoint);
+    $json_arr = array("userID"=>$id,"userName"=>$userName,"userSex"=>$userSex,"userAge"=>$userAge,"userPoint"=>$userPoint);
     echo json_encode($json_arr);
     exit;
 ?>
