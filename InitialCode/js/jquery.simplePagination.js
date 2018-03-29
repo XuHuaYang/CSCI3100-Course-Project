85aeb0412cadb02dc9b9f6d0bccbfa48ec3e2416
Sleepy-Neko <31822024+Sleepy-Neko@users.noreply.github.com>
2018-03-29 21:40:09 +0800

    Add files via upload
    for buyer_page

---
+++ InitialCode/js/jquery.simplePagination.js
@@ -0,0 +1,253 @@
+/****************************************************************
+ *																*		
+ * 						      代码库							*
+ *                        www.dmaku.com							*
+ *       		  努力创建完善、持续更新插件以及模板			*
+ * 																*
+****************************************************************/
+/**
+* simplePagination.js v1.5
+* A simple jQuery pagination plugin.
+* http://flaviusmatis.github.com/simplePagination.js/
+*
+* Copyright 2012, Flavius Matis
+* Released under the MIT license.
+* http://flaviusmatis.github.com/license.html
+*/
+(function($){
+
+	var methods = {
+		init: function(options) {
+			var o = $.extend({
+				items: 1,
+				itemsOnPage: 1,
+				pages: 0,
+				displayedPages: 5,
+				edges: 2,
+				currentPage: 1,
+				hrefTextPrefix: '#page-',
+				hrefTextSuffix: '',
+				prevText: '<<',
+				nextText: '>>',
+				ellipseText: '&hellip;',
+				cssStyle: 'light-theme',
+				selectOnClick: true,
+				onPageClick: function(pageNumber, event) {
+					// Callback triggered when a page is clicked
+				$("#records").empty();
+
+				var first_line = $("<tr><th>QID</th><th>Title</th><th>Topic</th><th>Credits</th><th></th></tr>");
+	            first_line.appendTo($("#records"));  
+				for(var i=(pageNumber-1)*6; i<pageNumber*6; i++)
+	            {
+	                var record= $("<tr><td>"+ (i+1) + "</td><td>A Simple Psychological Quesionnare1</td><td>Business</td><td>100</td><td><button type=\"submit\">View</button></td></tr>");
+	                record.appendTo($("#records"));
+
+	            }
+					// Page number is given as an optional parameter
+				},
+				onInit: function() {
+					
+				var first_line = $("<tr><th>QID</th><th>Title</th><th>Topic</th><th>Credits</th><th></th></tr>");
+	            first_line.appendTo($("#records"));  
+				for(var i=0; i<6; i++)
+	            {
+	                var record= $("<tr><td>"+ (i+1) + "</td><td>A Simple Psychological Quesionnare1</td><td>Business</td><td>100</td><td><button type=\"submit\">View</button></td></tr>");
+	                record.appendTo($("#records"));
+
+	            }
+				
+				}
+			}, options || {});
+
+			var self = this;
+
+			o.pages = o.pages ? o.pages : Math.ceil(o.items / o.itemsOnPage) ? Math.ceil(o.items / o.itemsOnPage) : 1;
+			o.currentPage = o.currentPage - 1;
+			o.halfDisplayed = o.displayedPages / 2;
+
+			this.each(function() {
+				self.addClass(o.cssStyle + ' simple-pagination').data('pagination', o);
+				methods._draw.call(self);
+			});
+
+			o.onInit();
+
+			return this;
+		},
+
+		selectPage: function(page) {
+			methods._selectPage.call(this, page - 1);
+			return this;
+		},
+
+		prevPage: function() {
+			var o = this.data('pagination');
+			if (o.currentPage > 0) {
+				methods._selectPage.call(this, o.currentPage - 1);
+			}
+			return this;
+		},
+
+		nextPage: function() {
+			var o = this.data('pagination');
+			if (o.currentPage < o.pages - 1) {
+				methods._selectPage.call(this, o.currentPage + 1);
+			}
+			return this;
+		},
+
+		getPagesCount: function() {
+			return this.data('pagination').pages;
+		},
+
+		getCurrentPage: function () {
+			return this.data('pagination').currentPage + 1;
+		},
+
+		destroy: function(){
+			this.empty();
+			return this;
+		},
+
+		redraw: function(){
+			methods._draw.call(this);
+			return this;
+		},
+
+		disable: function(){
+			var o = this.data('pagination');
+			o.disabled = true;
+			this.data('pagination', o);
+			methods._draw.call(this);
+			return this;
+		},
+
+		enable: function(){
+			var o = this.data('pagination');
+			o.disabled = false;
+			this.data('pagination', o);
+			methods._draw.call(this);
+			return this;
+		},
+
+		_draw: function() {
+			var	o = this.data('pagination'),
+				interval = methods._getInterval(o),
+				i;
+
+			methods.destroy.call(this);
+
+			var $panel = this.prop("tagName") === "UL" ? this : $('<ul></ul>').appendTo(this);
+
+			// Generate Prev link
+			if (o.prevText) {
+				methods._appendItem.call(this, o.currentPage - 1, {text: o.prevText, classes: 'prev'});
+			}
+
+			// Generate start edges
+			if (interval.start > 0 && o.edges > 0) {
+				var end = Math.min(o.edges, interval.start);
+				for (i = 0; i < end; i++) {
+					methods._appendItem.call(this, i);
+				}
+				if (o.edges < interval.start && (interval.start - o.edges != 1)) {
+					$panel.append('<li class="disabled"><span class="ellipse">' + o.ellipseText + '</span></li>');
+				} else if (interval.start - o.edges == 1) {
+					methods._appendItem.call(this, o.edges);
+				}
+			}
+
+			// Generate interval links
+			for (i = interval.start; i < interval.end; i++) {
+				methods._appendItem.call(this, i);
+			}
+
+			// Generate end edges
+			if (interval.end < o.pages && o.edges > 0) {
+				if (o.pages - o.edges > interval.end && (o.pages - o.edges - interval.end != 1)) {
+					$panel.append('<li class="disabled"><span class="ellipse">' + o.ellipseText + '</span></li>');
+				} else if (o.pages - o.edges - interval.end == 1) {
+					methods._appendItem.call(this, interval.end++);
+				}
+				var begin = Math.max(o.pages - o.edges, interval.end);
+				for (i = begin; i < o.pages; i++) {
+					methods._appendItem.call(this, i);
+				}
+			}
+
+			// Generate Next link
+			if (o.nextText) {
+				methods._appendItem.call(this, o.currentPage + 1, {text: o.nextText, classes: 'next'});
+			}
+		},
+
+		_getInterval: function(o) {
+			return {
+				start: Math.ceil(o.currentPage > o.halfDisplayed ? Math.max(Math.min(o.currentPage - o.halfDisplayed, (o.pages - o.displayedPages)), 0) : 0),
+				end: Math.ceil(o.currentPage > o.halfDisplayed ? Math.min(o.currentPage + o.halfDisplayed, o.pages) : Math.min(o.displayedPages, o.pages))
+			};
+		},
+
+		_appendItem: function(pageIndex, opts) {
+			var self = this, options, $link, o = self.data('pagination'), $linkWrapper = $('<li></li>'), $ul = self.find('ul');
+
+			pageIndex = pageIndex < 0 ? 0 : (pageIndex < o.pages ? pageIndex : o.pages - 1);
+
+			options = $.extend({
+				text: pageIndex + 1,
+				classes: ''
+			}, opts || {});
+
+			if (pageIndex == o.currentPage || o.disabled) {
+				if (o.disabled) {
+					$linkWrapper.addClass('disabled');
+				} else {
+					$linkWrapper.addClass('active');
+				}
+				$link = $('<span class="current">' + (options.text) + '</span>');
+			} else {
+				$link = $('<a href="' + o.hrefTextPrefix + (pageIndex + 1) + o.hrefTextSuffix + '" class="page-link">' + (options.text) + '</a>');
+				$link.click(function(event){
+					return methods._selectPage.call(self, pageIndex, event);
+				});
+			}
+
+			if (options.classes) {
+				$link.addClass(options.classes);
+			}
+
+			$linkWrapper.append($link);
+
+			if ($ul.length) {
+				$ul.append($linkWrapper);
+			} else {
+				self.append($linkWrapper);
+			}
+		},
+
+		_selectPage: function(pageIndex, event) {
+			var o = this.data('pagination');
+			o.currentPage = pageIndex;
+			if (o.selectOnClick) {
+				methods._draw.call(this);
+			}
+			return o.onPageClick(pageIndex + 1, event);
+		}
+
+	};
+	
+	$.fn.pagination = function(method) {
+
+		// Method calling logic
+		if (methods[method] && method.charAt(0) != '_') {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' +  method + ' does not exist on jQuery.pagination');
+		}
+
+	};
+
+})(jQuery);console.log("\u002f\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u000d\u000a\u0020\u002a\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u0009\u0009\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0020\u0020\u0020\u0020\u0020\u0020\u4ee3\u7801\u5e93\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0077\u0077\u0077\u002e\u0064\u006d\u0061\u006b\u0075\u002e\u0063\u006f\u006d\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0009\u0009\u0020\u0020\u52aa\u529b\u521b\u5efa\u5b8c\u5584\u3001\u6301\u7eed\u66f4\u65b0\u63d2\u4ef6\u4ee5\u53ca\u6a21\u677f\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002f");
